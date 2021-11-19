8d830266a9c8bbfd2281765203359168f39067f2
Marie Askjem <marie@getacademy.no>
2021-11-19 12:11:59 +0100

    awd

---
+++ SodaMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SodaMachineProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sodaMachine = new SodaMachine();
+        }
+    }
+}
